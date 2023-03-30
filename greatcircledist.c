da7a4f315f3392f505c74a5464c4b154ddd76a0c
E. Dronkert <ednl@users.noreply.github.com>
2023-03-30 16:24:07 +0200

    Huge simplification

--- greatcircledist.c
+++ greatcircledist.c
@@ -17,105 +17,81 @@
 #include <string.h>   // strlen
 #include <math.h>     // sin, cos, asin, sqrt
 #include <errno.h>    // errno, ERANGE
-#include <stdbool.h>  // bool
 
-#define E_NUMARG  1
-#define E_INVALID 2
-#define E_RANGE   3
-#define E_LAT90   4
-#define E_LON180  5
+// Error exit codes
+#define ERR_NUMARG  1  // number of arguments must be 4
+#define ERR_INVALID 2  // whole argument must be a fp number
+#define ERR_RANGE   3  // argument must be a valid double
+#define ERR_LAT90   4  // latitude must be between -90 and +90
+#define ERR_LON180  5  // longitude must be between -180 and +180
 
-#define EPS  1e-8  // epsilon for double comparison (range [-180,+180])
-#define D2R  1.74532925199432953e-2  // pi/180 (degrees to radians)
-#define D2RH 8.72664625997164656e-3  // pi/360 (degrees to radians, half)
+// Distance calculation constants
+#define DEG2RAD   1.74532925199432953e-2  // pi/180 (degrees to radians)
+#define RADIUS_P  6.357e+6                // earth polar radius in metres
+#define RADIUS_E  6.378e+6                // earth equatorial radius in metres
+#define RADIUS_P2 (RADIUS_P * RADIUS_P)   // square polar radius
+#define RADIUS_E2 (RADIUS_E * RADIUS_E)   // square equatorial radius
 
-#define R_POL   6.357e+6         // earth polar radius in metres
-#define R_EQT   6.378e+6         // earth equatorial radius in metres
-#define R_POL2  (R_POL * R_POL)  // square polar radius
-#define R_EQT2  (R_EQT * R_EQT)  // square equatorial radius
-
-// Two lat/lon points in degrees
-// = four doubles addressable as either deg[0..3] or lat1,lon1,lat2,lon2
+// Line segment on the Earth surface defined by 2 lat/lon points
+// = four doubles addressable as either coor[0..3] or lat1,lon1,lat2,lon2
 typedef union {
-    double deg[4];
+    double coor[4];
     struct { double lat1, lon1, lat2, lon2; };
-} Segment;
-
-static bool equal(const double a, const double b)
-{
-    return fabs(a - b) <= EPS;
-}
-
-// Local earth diameter in metres, from latitude in degrees.
-// Ref.: https://en.wikipedia.org/wiki/Earth_radius#Location-dependent_radii
-static double local_earth_diameter(const double latdeg)
-{
-    // if (equal(latdeg, 0))
-    //     return 2 * R_EQT;
-    // if (equal(latdeg, 90) || equal(latdeg, -90))
-    //     return 2 * R_POL;
-    double lat = latdeg * D2R;
-    double s = sin(lat), c = cos(lat);
-    double rs2 = R_POL2 * s * s;
-    double rc2 = R_EQT2 * c * c;
-    return 2 * sqrt((R_POL2 * rs2 + R_EQT2 * rc2) / (rs2 + rc2));
-}
-
-// Great circle distance in metres, using local earth radius and
-// inverse haversine function, for 2 lat/lon points in degrees.
-// Ref.: https://en.wikipedia.org/wiki/Haversine_formula#Formulation
-static double great_circle_distance(const Segment a)
-{
-    double dlat = a.lat2 - a.lat1;
-    double dlon = a.lon2 - a.lon1;
-    // bool eqlat = equal(dlat, 0);
-    // bool eqlon = equal(dlon, 0);
-
-    // if (eqlat && eqlon)
-    //     return 0;
-
-    // if (eqlat)
-    //     return local_earth_diameter(a.lat1) * asin(fabs(cos(a.lat1 * D2R) * sin(dlon * D2RH)));
-
-    double D = local_earth_diameter((a.lat1 + a.lat2) / 2);
-    // if (eqlon)
-    //     return D * asin(fabs(sin(dlat * D2RH)));
-
-    double slat = sin(dlat * D2RH);  // sin lat diff
-    double slon = sin(dlon * D2RH);  // sin lon diff
-    return D * asin(sqrt(slat * slat + cos(a.lat1 * D2R) * cos(a.lat2 * D2R) * slon * slon));
-}
+} LineSegment;
 
 int main(int argc, char *argv[])
 {
+    // Number of arguments = 1 (program name) + 4 (command line arguments)
     if (argc != 5) {
         fprintf(stderr, "Provide 4 arguments: lat1 lon1 lat2 lon2.\n");
-        exit(E_NUMARG);
+        exit(ERR_NUMARG);
     }
 
-    Segment a = {0};
-    for (int i = 0, j = 1; i < 4; ++i, ++j) {
+    LineSegment a = {0};
+    for (int i = 0, j = 1; i < 4; ++i, ++j) {  // value index i, argument index j
+        // Parse string argument to double
         char *end;
-        a.deg[i] = strtod(argv[j], &end);  // parse to double
+        a.coor[i] = strtod(argv[j], &end);
+        // Whole string used?
         if ((size_t)(end - argv[j]) != strlen(argv[j])) {
             fprintf(stderr, "Not a number: %s.\n", argv[j]);
-            exit(E_INVALID);
+            exit(ERR_INVALID);
         }
+        // Representable as double?
         if (errno == ERANGE) {
             fprintf(stderr, "Out of range: %s.\n", argv[j]);
-            exit(E_RANGE);
+            exit(ERR_RANGE);
         }
-        if ((i == 0 || i == 2) && (a.deg[i] < -90 || a.deg[i] > 90)) {
+        // Arg 1 and 3 (values 0 and 2) are latitudes in degrees [-90,+90]
+        if ((i == 0 || i == 2) && (a.coor[i] < -90 || a.coor[i] > 90)) {
             fprintf(stderr, "Latitude must be between -90 and +90: %s.\n", argv[j]);
-            exit(E_LAT90);
+            exit(ERR_LAT90);
         }
-        if ((i == 1 || i == 3) && (a.deg[i] < -180 || a.deg[i] > 180)) {
+        // Arg 2 and 4 (values 1 and 3) are longitudes in degrees [-180,+180]
+        if ((i == 1 || i == 3) && (a.coor[i] < -180 || a.coor[i] > 180)) {
             fprintf(stderr, "Longitude must be between -180 and +180: %s.\n", argv[j]);
-            exit(E_LON180);
+            exit(ERR_LON180);
         }
+        // From degrees to radians
+        a.coor[i] *= DEG2RAD;
     }
 
-    // Distance in m, precision to cm
-    printf("%.2f\n", great_circle_distance(a));
+    // Intermediate values for local earth radius
+    // Ref.: https://en.wikipedia.org/wiki/Earth_radius#Location-dependent_radii
+    double avglat = (a.lat1 + a.lat2) / 2;
+    double s = sin(avglat);
+    double c = cos(avglat);
+    double rs = RADIUS_P2 * s * s;
+    double rc = RADIUS_E2 * c * c;
+
+    // Intermediate values for inverse haversine function
+    // Ref.: https://en.wikipedia.org/wiki/Haversine_formula#Formulation
+    double slat = sin((a.lat2 - a.lat1) / 2);
+    double slon = sin((a.lon2 - a.lon1) / 2);
+
+    // Great circle distance in m, precision to cm
+    printf("%.2f\n", 2 * sqrt((RADIUS_P2 * rs + RADIUS_E2 * rc) / (rs + rc))
+        * asin(sqrt(slat * slat + cos(a.lat1) * cos(a.lat2) * slon * slon)));
+
     return 0;
 }
